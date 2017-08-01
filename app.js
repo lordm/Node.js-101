b970327f771cc679c04bfcdc0f7c69e02b218343
Marwan Osman <lordm2005@gmail.com>
2017-08-01 14:22:10 +0200

    Updated 2017 with electron

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888, '127.0.0.1');
+}).listen(1200, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://127.0.0.1:1200/');
